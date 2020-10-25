51a92c54f33c6aad006692d12342cf217c3ef720
Simon <simonenkoivansimon@gmail.com>
2020-10-25 15:34:06 +0400

    Поиск сегментов по заданому цветовому диапазону в BGR, на примере рыбки Немо

--- nemo.py
+++ nemo.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+# Программа для поиска сегментов по заданому цветовому диапазону в BGR, на примере рыбки Немо
+"""Основной код из статьи: https://pythonru.com/biblioteki/segmentacija-izobrazhenija-s-opencv-i-python"""
+
+
 import cv2
-import numpy as np
 
 
-# Функции для удобства
+# Функция для удобства
 def view_image(name_of_window, image):
     """Функция вывода изображения"""
     cv2.namedWindow(name_of_window, cv2.WINDOW_NORMAL)
@@ -11,21 +17,34 @@ def view_image(name_of_window, image):
     cv2.destroyAllWindows()
 
 
-def change_size(image, percent: int):
-    """Функция изменения размера изображения, чтобы оно выводилось корректно"""
-    width = int(img.shape[1] * percent / 100)
-    height = int(img.shape[0] * percent / 100)
-    dim = (width, height)
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-    return resized
+# Считываем картинку (точно такой же Немо, что из статьи)
+img = cv2.imread('./images/nemo.png')
 
+"""Нашел другого Немо, у которого слегка отличается оранжевый цвет"""
+# img = cv2.imread('./images/nemo_real.jpg')
+"""
+Результат: неверно выбирает сегмент
+Причина: оранжевый оттенок на картинке не подходит под заданный нами диапазон
+"""
 
-# Считывакм и переводим в
-img = cv2.imread('images/Nil.jpg')
+view_image("normal RGB image", img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+view_image("BGR image", img)
 
-# dark_green = (0, 30, 0)     # Вполне различимый зелёный
-# light_green = (0, 255, 0)   # Супер-мега ярко зелёный
-#
-# mask = cv2.inRange(hsv_nemo, light_orange, dark_orange)
+# Переводим в HSV
+hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+view_image("hsv image", hsv_img)
 
-view_image("test", img)
+# Диапазон оранжевого цвета в BGR, если хотим искать сегменты с оттенками оражевого
+light_col = (1, 190, 200)
+dark_col = (18, 255, 255)
+
+# Когда необходимый цветовой диапазон выбран, можно использовать cv2.inRange(), чтобы разбить изображение
+mask = cv2.inRange(hsv_img, light_col, dark_col)
+
+# Чтобы наложить маску поверх оригинального изображения, используется cv2.bitwise_and()
+result = cv2.bitwise_and(hsv_img, hsv_img, mask=mask)
+
+# Выводим изображения
+view_image("mask", mask)
+view_image("result", result)
